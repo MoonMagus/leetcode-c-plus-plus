e28c87303adeb60a2c2c99cc69fb57ee2df01611
MoonMagus <935865579@qq.com>
2020-10-18 01:02:10 +0800

    Search in Rotated Sorted Array II.

---
+++ leetcode/Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,82 @@
+#pragma warning(disable:4996)
+#include <cstdio>
+#include <iostream> 
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target)
+                return true;
+
+            if (nums[l] == nums[m] && nums[r] == nums[m]) {
+                ++l;
+                --r;
+            }
+            else if (nums[m] > nums[l] || nums[m] > nums[r]) {
+                // [left, mid] increase.
+                if (target >= nums[l] && target < nums[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            }
+            else {
+                // [mid, right] increase.
+                if (target > nums[m] && target <= nums[r])
+                    l = m + 1;
+                else
+                    r = m - 1;
+            }
+        }
+
+        return false;
+    }
+};
+
+vector<vector<int>> initializeData(int lineCount = 1) {
+    freopen("C:\\Users\\fengy\\Desktop\\io\\in.txt", "r", stdin); //输入重定向，输入数据将从in.txt文件中读取   
+    freopen("C:\\Users\\fengy\\Desktop\\io\\\\out.txt", "w", stdout); //输出重定向，输出数据将保存在out.txt文件中   
+    vector<vector<int>> v = vector<vector<int>>(lineCount);
+
+    // Read data from text file.
+    string line;
+    for (int i = 0; i < lineCount && getline(cin, line); ++i) {
+        istringstream iss(line);
+        int number;
+        while (iss >> number)
+            v[i].push_back(number);
+    }
+
+    return v;
+}
+
+void releaseData() {
+    fclose(stdin);         //关闭文件   
+    fclose(stdout);        //关闭文件   
+}
+
+void printData(vector<vector<int>>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = 0; j < v[i].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> v = initializeData();
+
+    Solution so;
+    int index = so.search(v[0], 3);
+    cout << index << endl;
+
+    releaseData();
+    return 0;
+}
